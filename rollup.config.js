9b04e4cf913d8bb2ae46b47369de7d3bc6264541
William McMurray <tiwilinlove@hotmail.com>
2020-10-27 23:49:48 -0400

    Add basic project files

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import { terser } from 'rollup-plugin-terser'
+import copy from 'rollup-plugin-copy'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: [
+    // UMD build (browser-friendly)
+    {
+      file: pkg.browser,
+      format: 'umd',
+      name: 'createGameLoop',
+    },
+
+    // CommonJS build (for Node)
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'default',
+    },
+
+    // ES module build (for bundlers)
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  plugins: [
+    // terser(),
+    copy({
+      targets: [
+        // move the UMD build into the docs folder (so it can be used by the live demo)
+        {
+          src: pkg.browser,
+          dest: 'docs/js',
+        },
+      ],
+    }),
+  ],
+};
